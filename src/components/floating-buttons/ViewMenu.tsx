ab1ab8f45ceac962be9dbadd43ec4ec6d510b0cf
jibinshan <jibinshanu23@gmail.com>
2025-01-25 13:46:40 +0530

    Add new SVG images, implement service charge calculation, and enhance menu organization

--- src/components/floating-buttons/ViewMenu.tsx
+++ src/components/floating-buttons/ViewMenu.tsx
@@ -2,14 +2,14 @@ import { Icons } from "@/components/Icon";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-const ViewMenu = ({}) => {
+const ViewMenu = ({ }) => {
   return (
     <Button
       asChild
-      className="z-50 flex aspect-square h-24 flex-col items-center justify-center rounded-full bg-primary px-0 py-0 text-center text-xs uppercase text-black hover:bg-[#1D1D1D]"
+      className="z-50 flex aspect-square h-24 flex-col items-center justify-center rounded-full bg-primary px-0 py-0 text-center text-xs uppercase text-white hover:bg-[#1D1D1D]"
     >
       <Link href="/table-booking">
-        <Icons.dining color="black" />
+        <Icons.dining color="white" />
         Table
         <br />
         Booking
